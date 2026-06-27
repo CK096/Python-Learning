015b73d2a38080cf2d18ea69948a0e68ae9ec07e
CKeat0906 <chunkeat990906@gmail.com>
2026-06-27 12:20:15 +0800

    Update Student_Management_System.py

--- Student_Management_System.py
+++ Student_Management_System.py
@@ -3,6 +3,17 @@ students = {}
 def search_students():
     for name, mark in students.items():
         print(f"{name} : {mark}")
+    print("------------")
+
+def no_rekod_student():
+    if not students:
+        print("Rekod Is Empty!")
+        return True
+
+def not_found_student(name):
+    if name not in students:
+        print("This Student Name Invalid!")
+        return True
 
 
 while True:
@@ -13,7 +24,7 @@ while True:
                             "4. Update Score\n"
                             "5. Delete Student\n"
                             "6. Exit\n"
-                            "------------\n"
+                            "\n"
                             "Choose: "))
     except ValueError:
         print("Please Select Correct Choice")
@@ -21,17 +32,53 @@ while True:
 
     if choice == 1:
         student = input("Key In Student Name: ").capitalize()
+        if student == "" or student == " ":
+            continue
         students[student] = 0
+
 
     elif choice == 2:
         search_students()
+        no_rekod_student()
 
     elif choice == 3:
+        if no_rekod_student():
+            continue
         search = input("Search Student Name: ").capitalize()
         if search in students:
             print("Have This Student")
-        else:
-            print("Not found")
+        not_found_student(search)
+
+    elif choice == 4:
+        if no_rekod_student():
+            continue
+        search_students()
+        name = input("Select Student: ").capitalize()
+        if not_found_student(name):
+            continue
+        score = int(input("Update Student Score: "))
+        if 0 > score or score > 100:
+            print("Score minimun is 0 and maximun is 100 ")
+            continue
+        students.update({name:score})
+        print(f"Update {name} Score Complete")
+
+    elif choice == 5:
+        if no_rekod_student():
+            continue
+        search_students()
+        delete = input("Select student U want delete: ").capitalize()
+        if not_found_student(delete):
+            continue
+        del students[delete]
+        print("Student Rekod Is Delete!!!")
+
+    elif choice == 6:
+        print("Thanks For Using")
+        break
+
+
+
 
 
 
