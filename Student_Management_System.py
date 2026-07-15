4fdda97016c97c894f40e4cfc86f6458b943140a
CKeat0906 <chunkeat990906@gmail.com>
2026-07-15 14:45:37 +0800

    Update Student_Management_System.py

--- Student_Management_System.py
+++ Student_Management_System.py
@@ -1,5 +1,21 @@
 students = {}
+path_file = r"C:\Users\user\Desktop\workshop\students.txt"
 
+try:
+    with open(path_file,"r") as file: #打开文件
+        for line in file:
+            data = line.strip().split(",")
+            name = data[0]
+            score = int(data[1])
+            students[name] = score
+except FileNotFoundError: #如果找不到文件就Pass
+    print("First Time Running Program"
+
+def save_students(): #储存文件
+    with open(path_file, "w") as file:
+        for name, score in students.items():
+            file.write(f"{name},{score}\n")
+            
 def search_students(): #显示Dictionary
     for name, mark in students.items():
         print(f"{name} : {mark}")
@@ -29,17 +45,22 @@ while True:
                         "3. Search Student\n"
                         "4. Update Score\n"
                         "5. Delete Student\n"
-                        "6. Exit\n"
-                        "\n"
+                        "6. Show Highest Score\n"
+                        "7. Show Lowest Score\n"
+                        "8. Average Score\n"
+                        "9. Exit\n"
+                        "------------------------\n"
                         "Choose: ")
+
     if choice == 1:
         student = input("Key In Student Name: ").capitalize()
         if student in students:
             print("Student Already Exists")
             continue
-        elif student.strip() == "":
+        elif not student.strip():
             continue
-        students[student] = 0 #加入Dictionary 里面
+        students[student] = 0 #加入Dictionary里面
+        save_students()
 
 
     elif choice == 2:
@@ -65,10 +86,11 @@ while True:
             continue
         score = error_key(("Update Student Score: "))
         if 0 > score or score > 100:
-            print("Score minimun is 0 and maximun is 100 ")
+            print("Score Min is 0 and Max is 100 ")
             continue
-        students.update({name:score}) #直接更新某段Dictionary,如果name一样的，就更新Score
-        print(f"Update {name} Score {score} is Complete")
+        students[name] = score #直接更新某段Dictionary,如果name一样的，就更新Score
+        save_students()
+        print(f"Update {name} Score Complete")
 
     elif choice == 5:
         if no_rekod_student():
@@ -78,8 +100,42 @@ while True:
         if not_found_student(delete):
             continue
         del students[delete] #直接删除Dictionary 里的某段
+        save_students()
         print("Student Rekod Is Delete!!!")
 
     elif choice == 6:
+        if no_rekod_student():
+            continue
+        search_students()
+        highest = max(students.values())
+        for name, score in students.items():
+            if score == highest:
+                print(f"Highest Score is {name}:{score}")
+
+    elif choice == 7:
+        if no_rekod_student():
+            continue
+        search_students()
+        lowest = min(students.values())
+        for name, score in students.items():
+            if score == lowest:
+                print(f"Lowest Score is {name}:{score}")
+
+    elif choice == 8:
+        if no_rekod_student():
+            continue
+        search_students()
+        total = sum(students.values())
+        qty = len(students)
+        print(f"Average Score is {total / qty:.2f}")
+
+    elif choice == 9:
         print("Thanks For Using")
         break
+
+
+
+
+
+
+
