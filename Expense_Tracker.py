88572f7930aa6cbbb5486e00facb1466ebc46fa2
CKeat0906 <chunkeat990906@gmail.com>
2026-07-17 17:25:04 +0800

    Update Expense_Tracker.py

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -1,4 +1,21 @@
 expenses = []
+path_file = r"C:\Users\user\Desktop\workshop\expenses_record"
+
+def load_expense():
+    try:
+        with open(path_file, "r") as file:
+            for line in file:
+                data = line.strip().split(",")
+                category = data[0]
+                amount = float(data[1])
+                expenses.append({"category": category,"amount" : amount})
+    except FileNotFoundError:
+        pass
+
+def save_expense():
+    with open(path_file,"w") as file:
+        for expense in expenses:
+            file.write(f"{expense['category']},{expense['amount']}\n")
 
 def add_expense():
     category = input("Type Of Category Expense:").strip().title()
@@ -7,9 +24,11 @@ def add_expense():
 
 def view_expense():
     for index, expense in enumerate(expenses):
-        print(f"{index+1}. {expense.get('category')} RM {expense.get('amount'):.2f}")
+        print(f"{index + 1}. {expense['category']} RM {expense['amount']:.2f}")
 
 
 while True:
-    add_expense()
+    load_expense()
     view_expense()
+    add_expense()
+    save_expense()
