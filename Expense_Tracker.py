36f4470bb21866dec4a9c682f380f939aa9252d6
CKeat0906 <chunkeat990906@gmail.com>
2026-07-20 16:29:53 +0800

    Update Expense_Tracker.py

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -1,6 +1,3 @@
-expenses = []
-path_file = r"C:\Users\user\Desktop\workshop\expenses_record"
-
 def load_expense():
     try:
         with open(path_file, "r") as file:
@@ -20,15 +17,64 @@ def save_expense():
 def add_expense():
     category = input("Type Of Category Expense:").strip().title()
     amount = float(input("How Many you use?: RM "))
-    expenses.append({"category" : category, "amount": amount})
+    expenses.append({"category": category, "amount": amount})
+    print("Add in Success!")
 
 def view_expense():
     for index, expense in enumerate(expenses):
-        print(f"{index + 1}. {expense['category']} RM {expense['amount']:.2f}")
+        print(f"{index + 1}. {expense['category']} : RM {expense['amount']:.2f}")
 
+def error_key(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Invalid Number")
+
+expenses = []
+path_file = r"C:\Users\user\Desktop\workshop\expenses_record.txt"
+load_expense()
 
 while True:
-    load_expense()
-    view_expense()
-    add_expense()
-    save_expense()
+    choice = error_key("=====Expense Tracker=====\n"
+                       "1.Add Expense\n"
+                       "2.View Expense\n"
+                       "3.Search Expense\n"
+                       "4.Delete Expense\n"
+                       "5.Total Expense\n"
+                       "6.Exit\n"
+                       "-------------------------\n"
+                       "Choice :")
+    if choice == 1:
+        add_expense()
+        save_expense()
+    elif choice == 2:
+        view_expense()
+    elif choice == 3:
+        found = False
+        search = input("what category expense you want to search: ").strip().title()
+        for index,result in enumerate(expenses):
+            if result["category"] == search:
+              found = True
+              print(f"{index + 1}.{result['category']} RM {result['amount']:.2f}")
+        if not found:
+            print("Category Not Found")
+
+    elif choice == 4:
+        view_expense()
+        delete = error_key("Select list item need to remove: ")
+        if delete < 1 or delete > len(expenses):
+            print("Invalid")
+            continue
+        del expenses[delete - 1]
+        print("Item list remove Success")
+        save_expense()
+    elif choice == 5:
+        total = 0
+        for expense in expenses:
+            total += expense["amount"]
+        print(f"Total is RM {total:.2f}")
+
+    elif choice == 6:
+        print("Thanks for using Expense Traker")
+        break
