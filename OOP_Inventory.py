ca645e5e429d11450fdc7b0426fafe9ee559c6eb
CKeat0906 <chunkeat990906@gmail.com>
2026-09-07 16:59:47 +0800

    Update OOP_Inventory.py

--- OOP_Inventory.py
+++ OOP_Inventory.py
@@ -6,15 +6,17 @@ class Product:
 
     def __str__(self):
         return (f"Name: {self.name}\n"
-                f"Price : RM{self.__price:.2f}\n"
-                f"Stock: {self.__stock}")
+                f"   Price : RM{self.__price:.2f}\n"
+                f"   Stock: {self.__stock}")
 
     def change_price(self,price):
         if price <= 0:
             print("Price Must Greater Than 0")
+            return False
         else:
             self.__price = price
             print(f"Price {self.name} Amend to RM{self.__price:.2f} Success!")
+            return True
 
     def add_stock(self,qty):
         if qty < 1:
@@ -50,12 +52,12 @@ class Inventory:
         self.products.append(product)
 
     def view_products(self):
-        for product in self.products:
-            print(product)
+        for index, product in enumerate(self.products):
+            print(f"{index+1}. {product}\n\n")
 
     def search_product(self,item):
         for product in self.products:
-            if item.title() == product.name:
+            if item.title().strip() == product.name:
                 print(product)
                 return True
 
@@ -64,7 +66,7 @@ class Inventory:
 
     def delete_product(self,item):
         for index,product in enumerate(self.products):
-            if item.title() == product.name:
+            if item.title().strip() == product.name:
                 self.products.pop(index)
                 print(f"{item} Delete Success")
                 return True
@@ -73,7 +75,7 @@ class Inventory:
         return False
 
     def edit_product_price(self,name,price):
-        name = name.title()
+        name = name.title().strip()
         for product in self.products:
             if name == product.name:
                 product.change_price(price)
@@ -83,7 +85,7 @@ class Inventory:
         return False
 
     def add_stock2 (self,name,stock):
-        name = name.title()
+        name = name.title().strip()
         for product in self.products:
             if name == product.name:
                 product.add_stock(stock)
@@ -93,7 +95,7 @@ class Inventory:
         return False
 
     def remove_stock(self,name,stock):
-        name = name.title()
+        name = name.title().strip()
         for product in self.products:
             if name == product.name:
                 if product.remove_stock(stock):
@@ -106,15 +108,44 @@ class Inventory:
         return False
 
 
-
-
-product1 = Product("Laptop",3500,10)
-product2 = Product("Mouse", 80, 20)
 inventory = Inventory()
 
-inventory.add_product(product1)
-inventory.add_product(product2)
-inventory.delete_product("Mouse")
-inventory.edit_product_price("Laptop",4000)
-inventory.remove_stock("Laptop",5)
-inventory.view_products()
+while True:
+    choice = int(input("===== Inventory Management System =====\n"
+                       "1. Add Product\n"
+                       "2. View Product\n"
+                       "3. Search Product\n"
+                       "4. Delete Product\n"
+                       "5. Edit Price\n"
+                       "6. Add Stock\n"
+                       "7. Remove Stock\n"
+                       "8. Exit\n"
+                       "Enter Your Choice: "))
+    if choice == 1:
+        name = input("Product Name: ").title().strip()
+        price = float(input("Product price: RM"))
+        stock = int(input("Stock Quantity: "))
+        product = Product(name,price,stock)
+        inventory.add_product(product)
+
+    elif choice == 2:
+        inventory.view_products()
+    elif choice == 3:
+        search = input("Key In Product You Search: ")
+        inventory.search_product(search)
+    elif choice == 4:
+        inventory.view_products()
+        delete = input("Key In Product You Want Remove: ")
+        inventory.delete_product(delete)
+    elif choice == 5:
+        name = input("Product Name: ")
+        price = float(input("Product Price: RM"))
+        inventory.edit_product_price(name,price)
+
+    elif choice == 6:
+        print("Add Stock")
+    elif choice == 7:
+        print("Remove Stock")
+    elif choice == 8:
+        print("Goodbye")
+        break
